495974e3172803d986482974f4565411746088cb
daniaaqel <dania.aqel@hotmail.com>
2022-09-10 11:29:36 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const GetMain = document.getElementById('main');
+GetMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Dania is the champion";
+
+const body = document.querySelector('body');
+body.appendChild(newHeader);
